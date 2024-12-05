6d38e2ac856c6fc9a5d7c25608963f265325d5ae
RenyBB <71499318+RenyBB@users.noreply.github.com>
2024-12-05 16:44:34 +0000

    Upload files

---
+++ stimulus_list.js
@@ -0,0 +1,153 @@
+// JavaScript source code
+
+var training_stimulus_list = [
+    {
+        "stimulus": "stimuli/standard_1400ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 1400,
+        "correct_response": "e" // e is for short 
+    },
+    {
+        "stimulus": "stimuli/standard_2600ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 2600,
+        "correct_response": "i" // i is for long
+    }
+]
+
+
+var standard_stimulus_list = [
+    {
+        "stimulus": "stimuli/standard_1400ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 1400
+    },
+    {
+        "stimulus": "stimuli/standard_1600ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 1600
+    },
+    {
+        "stimulus": "stimuli/standard_1800ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 1800
+    },
+    {
+        "stimulus": "stimuli/standard_2000ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 2000
+    },
+    {
+        "stimulus": "stimuli/standard_2200ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 2200
+    },
+    {
+        "stimulus": "stimuli/standard_2400ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 2400
+    },
+    {
+        "stimulus": "stimuli/standard_2600ms.wav",
+        "stimulus_type_id": 1,
+        "frequency": 1000,
+        "duration_ms": 2600
+    }
+]
+
+
+var deviant_stimulus_list = [
+    {
+        "stimulus": "stimuli/deviant_500hz_1400ms.wav",
+        "stimulus_type_id": 2,
+        "frequency": 500,
+        "duration_ms": 1400
+    },
+    {
+        "stimulus": "stimuli/deviant_500hz_1600ms.wav",
+        "stimulus_type_id": 2,
+        "frequency": 500,
+        "duration_ms": 1600
+    },
+    {
+        "stimulus": "stimuli/deviant_500hz_1800ms.wav",
+        "stimulus_type_id": 2,
+        "frequency": 500,
+        "duration_ms": 1800
+    },
+    {
+        "stimulus": "stimuli/deviant_500hz_2000ms.wav",
+        "stimulus_type_id": 2,
+        "frequency": 500,
+        "duration_ms": 2000
+    },
+    {
+        "stimulus": "stimuli/deviant_500hz_2200ms.wav",
+        "stimulus_type_id": 2,
+        "frequency": 500,
+        "duration_ms": 2200
+    },
+    {
+        "stimulus": "stimuli/deviant_500hz_2400ms.wav",
+        "stimulus_type_id": 2,
+        "frequency": 500,
+        "duration_ms": 2400
+    },
+    {
+        "stimulus": "stimuli/deviant_500hz_2600ms.wav",
+        "stimulus_type_id": 2,
+        "frequency": 500,
+        "duration_ms": 2600
+    },
+    {
+        "stimulus": "stimuli/deviant_2000hz_1400ms.wav",
+        "stimulus_type_id": 3,
+        "frequency": 500,
+        "duration_ms": 1400
+    },
+    {
+        "stimulus": "stimuli/deviant_2000hz_1600ms.wav",
+        "stimulus_type_id": 3,
+        "frequency": 500,
+        "duration_ms": 1600
+    },
+    {
+        "stimulus": "stimuli/deviant_2000hz_1800ms.wav",
+        "stimulus_type_id": 3,
+        "frequency": 500,
+        "duration_ms": 1800
+    },
+    {
+        "stimulus": "stimuli/deviant_2000hz_2000ms.wav",
+        "stimulus_type_id": 3,
+        "frequency": 500,
+        "duration_ms": 2000
+    },
+    {
+        "stimulus": "stimuli/deviant_2000hz_2200ms.wav",
+        "stimulus_type_id": 3,
+        "frequency": 500,
+        "duration_ms": 2200
+    },
+    {
+        "stimulus": "stimuli/deviant_2000hz_2400ms.wav",
+        "stimulus_type_id": 3,
+        "frequency": 500,
+        "duration_ms": 2400
+    },
+    {
+        "stimulus": "stimuli/deviant_2000hz_2600ms.wav",
+        "stimulus_type_id": 3,
+        "frequency": 500,
+        "duration_ms": 2600
+    }
+
+]
